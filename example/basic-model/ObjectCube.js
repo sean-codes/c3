604a3df44416d877c42d2bde99f50800be6d7d41
Sean Codes <sean_codes@outlook.com>
2020-11-27 20:32:46 -0600

    rename

---
+++ example/basic-model/ObjectCube.js
@@ -0,0 +1,12 @@
+import { c3 } from '../c3.js'
+
+export class ObjectCube extends c3.Object {
+   mesh() {
+      return this.c3.mesh.Box({ size: [1, 1, 1], color: '#FFF' })
+   }
+   
+   step() {
+      this.rotateX(0.01)
+      this.rotateZ(0.01)
+   }
+}
