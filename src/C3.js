1be286a1d7c51409f08d0eb01489f192d4a2dc01
Sean Codes <sean_codes@outlook.com>
2022-12-18 02:52:55 -0600

    animation timescale

--- src/C3.js
+++ src/C3.js
@@ -124,10 +124,13 @@ export class C3 {
    }
 
    engineStep() {
-      typeof window !== 'undefined' 
-         ? requestAnimationFrame(() => this.engineStep()) 
-         : setTimeout(() => this.engineStep(), this.engineSpeed)
-      // setTimeout(() => this.engineStep(), this.engineSpeed)
+      // rever later? animationframe >:(
+      const oneSec = 1000/60
+      const timeScale = 1/ (this.engineSpeed / oneSec)
+      setTimeout(() => this.engineStep(), this.engineSpeed)
+      // typeof window !== 'undefined' 
+      //    ? requestAnimationFrame(() => this.engineStep()) 
+      //    : setTimeout(() => this.engineStep(), this.engineSpeed)
          
       // I want to run this even if stopped
       this.gamepad.loop()
@@ -144,7 +147,7 @@ export class C3 {
       this.objects.loop(delta)
       
       this.physics.loop(delta)
-      this.models.loop(delta)
+      this.models.loop(delta*timeScale)
       this.keyboard.resetKeys()
       
       this.network.updateMetrics()
